a52f786ae3a9ef7f38f4128483907f5c59dacf1e
hyejj19 <89173923+hyejj19@users.noreply.github.com>
2021-11-04 22:55:24 +0900

    2021-1104

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function btnClick() {
-    const value = loginInput.value;
-    if (value ==="") {
-        alert("Please write your name");
-    }
+
+function onLoginSubmit(event) { //이벤트 리스너의 첫번째 인수는 이벤트에 대한 정보. event : 관행적으로 이렇게 지어줌.
+    event.preventDefault(); // 함수 실행시 브라우저에 기본적으로 행해지는 동작을 방지하는 함수, 이벤트 리스너의 첫번째 인자의 기능. 그러니까, submit을 실행시킬 때 발생하는 새로고침이라는 기본 기능을 막아주는 역할.
+    //const username = loginInput.value;
+    console.log(event);
 }
 
-loginButton.addEventListener("click", btnClick);
-
+loginForm.addEventListener("submit",onLoginSubmit)
