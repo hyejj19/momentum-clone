2998ad60d682457064220f0ff6b651411a1de63c
hyejj19 <89173923+hyejj19@users.noreply.github.com>
2021-11-14 13:49:51 +0900

    2021-1114 Created quotes.js

---
+++ js/quotes.js
@@ -0,0 +1,11 @@
+const quotes = [
+{quote: "왜 굳이 의미를 찾으려 하는가? 인생은 욕망이지 의미가 아니다."},
+{quote: "To know is nothing at all; to imagine is everything."},
+{quote: "The computer is only a fast idiot; it has no imagination; it cannot originate action. It is, and will remain, only a tool of man."},
+{quote: "The secret of business is to know something that nobody else knows."},
+{quote: "One man with courage makes a majority."},
+{quote: "Man is born to live, not to prepare for life. Life itself, the phenomenon of life, the gift of life,is so breathtakingly serious!"},
+{quote: "Anything you're good at contributes to happiness."},
+{quote: "Freedom is a system based on courage."},
+{quote: "If a man takes no thought about what is distant, he will find sorrow near at hand."},
+]
